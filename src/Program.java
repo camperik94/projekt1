a5cf02385671ae2c173b52179b35cbf10c1117d8
camperik94 <tomaszjach94@gmail.com>
2018-03-11 18:21:28 +0100

    poprawa2

--- src/Program.java
+++ src/Program.java
@@ -3,5 +3,9 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Witaj swiecie!");
 		System.out.println("Sprobujemy czeos nowego:P");
+		
+		if(1 != 2) {
+			System.out.println("Elo");
+		}
 	}
 }
